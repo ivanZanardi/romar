dcf7dac8bc635457d9d46187578b4b968c2e80ec
ivanZanardi <zanardi3@illinois.edu>
2025-02-10 21:48:11 -0600

    misc

--- examples/inputs/rad_on/max_mom_2_t2/allrun.sh
+++ examples/inputs/rad_on/max_mom_2_t2/allrun.sh
@@ -5,8 +5,8 @@ path_to_scripts=/home/zanardi/Codes/ML/ROMAr/romar/romar/scripts/
 echo -e "\nRunning 'build_rom' script ..."
 python -u $path_to_scripts/build_rom.py --inpfile build_rom.json
 
-# echo -e "\nRunning 'eval_rom_acc' script ..."
-# python -u $path_to_scripts/eval_rom_acc.py --inpfile eval_rom_acc.json
+echo -e "\nRunning 'eval_rom_acc' script ..."
+python -u $path_to_scripts/eval_rom_acc.py --inpfile eval_rom_acc.json
 
 echo -e "\nRunning 'visual_rom' script ..."
 python -u $path_to_scripts/visual_rom.py --inpfile visual_rom.json
