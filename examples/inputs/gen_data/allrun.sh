a229af5f881d941bde9a25f6cf7bffd6e44c91e9
ivanZanardi <zanardi3@illinois.edu>
2025-03-24 13:25:58 -0500

    misc

--- examples/inputs/gen_data/allrun.sh
+++ examples/inputs/gen_data/allrun.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -i
 
-path_to_scripts=/home/zanardi/Codes/ML/ROMAr/romar/romar/scripts/
+path_to_scripts=/u/zanardi3/Codes/ML/ROMAr/romar/romar/scripts/
 
 echo -e "\nRunning 'gen_data_train' script ..."
 python -u $path_to_scripts/gen_data_train.py --inpfile gen_data_train.json
 
 echo -e "\nRunning 'gen_data_test' script ..."
 python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test.json
+
+echo -e "\nRunning 'gen_data_test' script ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_rho.json
+
+echo -e "\nRunning 'gen_data_test' script ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_Thigh.json
+
+echo -e "\nRunning 'gen_data_test' script ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_Tlow.json
