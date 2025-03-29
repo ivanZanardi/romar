ea4f4b7a86a8b3f4ca17d2be4294b65ff064194a
ivanZanardi <zanardi3@illinois.edu>
2025-03-29 17:46:51 -0500

    misc

---
+++ examples/inputs/allrun.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -i
+
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+##SBATCH --exclude=ccc[0370-0385]
+##SBATCH --exclude=ccc[0400-0415]
+##SBATCH --nodelist=ccc[0370-0385]
+##SBATCH --nodelist=ccc[0400-0415]
+#SBATCH --job-name=run01
+#SBATCH --account=chess
+#SBATCH --partition=chess
+#SBATCH --output=log.output_%j
+#SBATCH --error=log.error_%j
+##SBATCH --mail-user=zanardi3@illinois.edu
+##SBATCH --mail-type=BEGIN,END
+
+# Activate the environment
+source /home/geek/Applications/ANACONDA/anaconda3/bin/activate
+conda activate sciml
+
+# Run the scripts
+cd gen_data
+bash allrun.sh
+cd ../
+
+cd max_mom_2
+bash allrun.sh
+cd ../
---
+++ examples/inputs/gen_data/allrun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -i
+
+path_to_scripts=/home/zanardi/Codes/ML/ROMAr/romar/romar/scripts/
+
+echo -e "\nRunning 'gen_data_train' script ..."
+python -u $path_to_scripts/gen_data_train.py --inpfile gen_data_train.json
+
+echo -e "\nRunning 'gen_data_test' script ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test.json
+
+echo -e "\nRunning 'gen_data_test' script (fixed rho) ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_rho_fixed.json
+
+echo -e "\nRunning 'gen_data_test' script (high temperatures) ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_temp_high.json
+
+echo -e "\nRunning 'gen_data_test' script (low temperatures) ..."
+python -u $path_to_scripts/gen_data_test.py --inpfile gen_data_test_temp_low.json
